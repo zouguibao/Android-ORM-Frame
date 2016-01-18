0cfe8da9790d67ba1c99c87897b8f4be9abbaeb6
zouguibao <zouguibao@163.com>
2016-01-18 17:57:10 +0800

    Revert "android orm frame"
    This reverts commit 51e7edc64753deb0e76ebcf825d45c5d79865442.

--- TheORMFrame/src/org/frame/database/db/OnUpdateListener.java
+++
@@ -1,7 +0,0 @@
-package org.frame.database.db;
-
-import android.database.sqlite.SQLiteDatabase;
-
-public interface OnUpdateListener {
-  void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion);
-}
